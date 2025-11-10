0b0751bd080a0394b7e972ecb3c6fe0467f7a383
Timofey <timof.kul@gmail.com>
2025-11-10 18:31:59 +0300

    redo

--- ide/blockly.js
+++ ide/blockly.js
@@ -305,17 +305,35 @@ bytecodeGenerator.forBlock['left'] = function(block, generator) {
 };
 
 const demoWorkspace = Blockly.inject('blocklyDiv', {
-  media: './blockly/media/',
-  toolbox: toolbox,
-  renderer: 'zelos',
-  zoom:
-    {controls: true,
-    wheel: true,
-    startScale: 0.8,
-    maxScale: 1,
-    minScale: 0.3,
-    scaleSpeed: 1.2,
-    pinch: true},
+	media: './blockly/media/',
+	toolbox: toolbox,
+	renderer: 'zelos',
+	// rtl: locale=="ar",
+	maxInstances:{
+		start:1
+	},
+	grid: {
+		spacing: 40,
+		snap: true,
+		length: 5,
+		colour: "#888",
+	},
+	zoom: {
+		controls: true,
+		startScale: 0.8,
+		maxScale: 1,
+		minScale: 0.1,
+		scaleSpeed: 1.5,
+		wheel: true,
+	},
+	move: {
+		scrollbars: {
+			horizontal: true,
+			vertical: true,
+		},
+		drag: true,
+		wheel: true,
+	},
 });
 
 progBuf=new Uint16Array(0);
